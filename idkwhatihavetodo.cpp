fac1add876b2ef50e270804f95cbb0b31c18afcf
Hugh O'Neill <2002hon@gmail.com>
2017-04-03 11:58:04 +0100

    Update idkwhatihavetodo.cpp

--- idkwhatihavetodo.cpp
+++ idkwhatihavetodo.cpp
@@ -1,5 +1,22 @@
-#include iostream
+#include <iostream>
 using namespace std;
-//this adds the libraries of what to do
+//this adds the libraries of the commands
 
-int main()
+int main() :
+{
+  cout << "How old are you?";
+//this asks the user how old they are
+   int Age;
+//creates a variable of the age so that the input can be processed
+  cin >> Age;
+//asks for an input of your age
+  if (Age >18) {
+  cout << "you cannot vote";
+  }
+//if the age is under 18, it tells you that you cannot vote
+else {
+    cout << "you can vote";
+  }
+//if the age is 18 or more, it tells you that you can vote
+  return 0;
+}
